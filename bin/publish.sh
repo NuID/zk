a8fda459a2736fce9c19ac7f0a3b4f215c1884bb
BJ Neilsen <bj.neilsen@gmail.com>
2021-03-11 16:41:55 -0500

    Inject brorand patch for self.crypto to built release script

--- bin/publish.sh
+++ bin/publish.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+out_file=target/node/nuid_zk.js
 rm -rf target
 shadow-cljs release node
-npm publish
+
+contents=$(cat $out_file)
+cat <<EOF > $out_file
+// Solve 'brorand' dependency on (global.)self.crypto
+import 'react-native-crypto'
+self.crypto = window.crypto
+// Original Build
+${contents}
+EOF
+
+# npm publish
