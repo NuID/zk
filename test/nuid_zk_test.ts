a549b11c6cd5a9ae79ba2ede6d59da53916fc17f
BJ Neilsen <bj.neilsen@gmail.com>
2021-05-07 13:07:01 -0400

    Add typescript definitions with js tests

---
+++ test/nuid_zk_test.ts
@@ -0,0 +1,23 @@
+import * as assert from 'assert'
+import * as zk from '../target/node/nuid_zk' // '@nuid/zk' uncomment for type checking
+
+describe('zk typescript definitions', () => {
+  it('supports roundtrip credential creation, challenging, and proving', () => {
+    const secret = 'secrets are good'
+    const verified = zk.verifiableFromSecret(secret)
+    assert.ok(verified)
+    const credential: zk.Credential = zk.credentialFromVerifiable(verified)
+    assert.ok(credential)
+    const challenge: zk.Challenge = zk.defaultChallengeFromCredential(credential)
+    assert.ok(challenge)
+    const proof: zk.Proof = zk.proofFromSecretAndChallenge(secret, challenge)
+    assert.ok(proof)
+    const foo = verified["nuid.zk/protocol"]
+    const verified2: zk.VerifiedCredential = zk.verifiableFromProofAndChallenge(proof, challenge)
+    assert.ok(verified2)
+    assert.strictEqual(
+      verified['nuid.zk.knizk/pub']['nuid.elliptic.curve/point'],
+      verified2['nuid.zk.knizk/pub']['nuid.elliptic.curve/point']
+    )
+  })
+})
